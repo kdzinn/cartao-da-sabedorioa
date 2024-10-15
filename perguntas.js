d31a25accc1ebf6af7f065837edd2f6afc34abad
kdzinn <141361307+kdzinn@users.noreply.github.com>
2024-10-15 17:16:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é cybersegurança?',
+    'E proteger computadores, redes, aplicações de software, sistemas essenciais e dados de possíveis ameaças digitais.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a Capital da russia?',
+    'A Capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um hacker?',
+    'Uma Pessoa com grandes conhecimentos de informática e programação.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vc e uma boa pessoa em Inglês?',
+    'Uma boa pessoa em ingles é a good person'
+)
+criaCartao(
+    'Historia',
+    'Quem proclamou independencia ou morte?',
+    'Foi Dom Pedro I gritou às margens do rio Ipiranga “independência ou morte'
+)
+criaCartao(
+    'Cybersegurança',
+    'Saber para que serve a Cybersegurança?',
+    'Evitar ataques que tentem acessar ou destruir dados, extorquir dinheiro ou interromper operações normais de negócios, sejam internas ou externas.'
+)
